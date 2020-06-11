fbe1577a68d1af66362b9be97e730defed0ebd7f
Vaibhav Mishra <64967666+VaibhavMishra98@users.noreply.github.com>
2020-06-11 12:48:27 +0530

    Add files via upload

---
+++ Migratory_birds.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the migratoryBirds function below.
+def migratoryBirds(arr,n):
+    arr_freq=[0,0,0,0,0,0]
+    for j in range(0,len(arr_freq)):
+        for i in range(0,n):
+            if(j == arr[i]):
+                arr_freq[j]=arr_freq[j]+1
+    k=max(arr_freq)
+    return (arr_freq.index(k))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr,arr_count)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
